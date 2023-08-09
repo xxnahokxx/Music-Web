f60a651de58e4384432304ac18a9a3bf1d8f0b2f
LucasRibotta <lucasribotta15@gmail.com>
2023-08-09 00:25:20 -0300

    fix landing estructura

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -1,20 +1,33 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import Image from 'next/image'
+import img from '@/assets/imagenLanding.jpg'
 
 const LandingPage = () => {
   return (
     <div>
-      <h1>Bienvenidos a mi página web</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure quas quae, iste, sit aspernatur repellendus mollitia cum, eaque ex illo consequatur suscipit adipisci? Ratione animi voluptatibus laudantium incidunt atque distinctio!</h2>
-      <img src="../../assets/imagenLanding.jpg" alt="perfil" />
-      <div>
-        <button>
-          <a href="/cursos"></a>
-        </button>
-        <button>
-          <a href="/music"></a>
-        </button>
+      <div className='flex flex-row text-slate-700 drop-shadow-md shadow-violet-600 text-3xl '>
+        <h1>Bienvenidos a mi página web</h1>
+        <Image className='h-[150px] w-[150px] rounded-md m-auto' src={img} alt="perfil" />
       </div>
+      <br />
+      <hr className='border-black' />
+      <div className='text-slate-500 text-2xl'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure quas quae, iste, sit aspernatur repellendus mollitia cum, eaque ex illo consequatur suscipit adipisci? Ratione animi voluptatibus laudantium incidunt atque distinctio!</p>
+      </div>
+      <br/>
+      <div className='flex flex-row m-auto justify-center'>
+        <div className='h-[40px] w-[70px] rounded-xl m-5 p-4 border-black bg-slate-400'>
+          <button>
+            <a href="/cursos">Cursos</a>
+          </button>
+        </div>
+        <div className='h-[40px] w-[70px] rounded-xl m-5 p-4 border-black bg-slate-400'>
+          <button>
+            <a href="/music">Música</a>
+          </button>
+        </div>
+      </div>
+
     </div>
   )
 }
