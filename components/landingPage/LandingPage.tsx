629800f54884b568602c9ac25740f096517d5bfb
LucasRibotta <lucasribotta15@gmail.com>
2023-08-08 23:54:04 -0300

    fix data router landing deleted -

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+      <h1>Bienvenidos a mi página web</h1>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure quas quae, iste, sit aspernatur repellendus mollitia cum, eaque ex illo consequatur suscipit adipisci? Ratione animi voluptatibus laudantium incidunt atque distinctio!</h2>
+      <img src="../../assets/imagenLanding.jpg" alt="perfil" />
+      <div>
+        <button>
+          <a href="/cursos"></a>
+        </button>
+        <button>
+          <a href="/music"></a>
+        </button>
+      </div>
+    </div>
   )
 }
 
